818d645fe8c605eb965b35b72049944ed15d781f
RobinFleige <robin.fleige@sap.com>
2018-05-14 01:13:51 +0200

    Moved getTotalFrequentRenterPoints to own method

--- src/classes/Customer.java
+++ src/classes/Customer.java
@@ -20,7 +20,6 @@ public class Customer {
 
 	public String statement() {
 		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration<Rental> enum_rentals = rentals.elements();
 		String result = "Rental Record for " + this.getName() + "\n";
 		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
@@ -28,10 +27,8 @@ public class Customer {
 		while (enum_rentals.hasMoreElements()) {
 			Rental each = (Rental) enum_rentals.nextElement();
 			double thisAmount = each.getCharge();
-			// add frequent renter points
-			frequentRenterPoints+=getFrequentRenterPoint(each);
 			// add bonus for a two day new release rental
-			
+
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -39,16 +36,24 @@ public class Customer {
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 		return result;
 	}
-	
-	public int getFrequentRenterPoint(Rental rental){
+
+	public int getFrequentRenterPoint(Rental rental) {
 		if ((rental.getMovie().getPrice() == Price.NEW_RELEASE) && rental.getDaysRented() > 1)
-				return 2;
+			return 2;
 		return 1;
 	}
 
-	
+	public int getTotalFrequentRenterPoints() {
+		int frequentRenterPoints = 0;
+		Enumeration<Rental> enum_rentals = rentals.elements();
+		while (enum_rentals.hasMoreElements()) {
+			Rental each = (Rental) enum_rentals.nextElement();
+			frequentRenterPoints+=getFrequentRenterPoint(each);
+		}
+		return frequentRenterPoints;
+	}
 
 }
